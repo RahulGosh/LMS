5062611a6aaa0eec2eb5fadb9b6108470075ff09
Yash Puniwala <yashpuniwala@gmail.com>
2025-03-26 12:48:35 +0530

    add changes in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,44 +31,9 @@ const corsOptions = {
 connectDB();
 app.use(cors(corsOptions));
 
-// const corsOptions ={
-//     origin: "*",
-//     credentials:true,
-// }
-// app.use(cors(corsOptions));
-
-// const prodOrigins = process.env.ORIGIN_1 || ''; // Default to an empty string if it's undefined
-// const devOrigins = "http://localhost:5173";
-// const allowedOrigins = process.env.NODE_ENV === "production" ? prodOrigins : devOrigins;
-
-// // Ensure `allowedOrigins` is an array of strings
-// const allowedOriginsArray = typeof allowedOrigins === 'string'
-//   ? allowedOrigins.split(',').map(origin => origin.trim())
-//   : []; // If undefined, fallback to empty array
-
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (origin && allowedOriginsArray.includes(origin)) {
-//             console.log("Origin allowed:", origin);
-//             callback(null, true);
-//         } else {
-//             console.log("Origin not allowed:", origin);
-//             callback(new Error("Not allowed by CORS"));
-//         }
-//     },
-//     credentials: true, // This allows credentials (cookies, authorization headers, etc.)
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-// }));
-
-app.use("/api/v1", user);
 app.use("/api/v1", course);
 app.use("/api/v1", coursePurchase);
 app.use("/api/v1", courseProgress);
 app.use("/api/v1", dashboard);
-
-app.use(express.static(path.join(DIRNAME, "/frontend/dist")))
-app.use("*", (_,res) => {
-    res.sendFile(path.resolve(DIRNAME, "frontend", "dist", "index.html"))
-})
 
 export default app;
